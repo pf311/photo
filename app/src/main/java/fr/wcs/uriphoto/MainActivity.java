4367db23e31a9a82a04cdedd9a26d007a2ed91f8
Pierre Fajardo <pierrefaja@gmail.com>
2018-12-10 12:01:29 +0100

    photo

---
+++ app/src/main/java/fr/wcs/uriphoto/MainActivity.java
@@ -0,0 +1,108 @@
+package fr.wcs.uriphoto;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_TAKE_PHOTO = 400;
+    private static final int REQUEST_GET_SINGLE_FILE = 100;
+    private static  final  String TYPE_IMAGE = "image/*";
+    private String mCurrentPhotoPath = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bPhoto = findViewById(R.id.photo);
+        Button bGallery = findViewById(R.id.buttonGallery);
+        bGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType(TYPE_IMAGE);
+                startActivityForResult(Intent.createChooser(intent, getString(R.string.select)), REQUEST_GET_SINGLE_FILE);
+            }
+        });
+        bPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Uri uri = null;
+            if (requestCode == REQUEST_GET_SINGLE_FILE) {
+                uri = data.getData();
+            }
+            if(requestCode == REQUEST_TAKE_PHOTO){
+                File file =new File(mCurrentPhotoPath);
+                uri = Uri.fromFile(file);
+            }
+            if (uri !=null){
+                ImageView photo = findViewById(R.id.imageView);
+                Glide.with(MainActivity.this).load(uri)
+                        .into(photo);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "fr.wcs.uriphoto.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+}
+
+
